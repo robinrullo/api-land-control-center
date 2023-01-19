8655616a3204329ac00bea9ce80a51a715f677b8
r-rullo <r-rullo@logitud.fr>
2023-01-19 15:16:36 +0100

    fix: api status update

--- src/pages/apis/InjectionStatus.tsx
+++ src/pages/apis/InjectionStatus.tsx
@@ -2,111 +2,119 @@ import {FC, useCallback, useEffect, useState} from "react";
 import {APICollectionResource} from "../../interfaces/api.interface";
 import {HttpClient} from "../../core/httpClient/HttpClient";
 import {
-    Button, DataTableSkeleton, InlineLoading,
-    Table, TableBody, TableCell, TableContainer,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableToolbar,
-    TableToolbarContent, Tag
+  Button, DataTableSkeleton, InlineLoading,
+  Table, TableBody, TableCell, TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableToolbar,
+  TableToolbarContent, Tag
 } from "carbon-components-react";
 import {
-    API_LAND_URL,
-    INJECTED_CHECK_REGEXP,
-    INJECTION_CHECK_INTERVAL,
-    WORKER_URL
+  API_LAND_URL,
+  INJECTED_CHECK_REGEXP,
+  INJECTION_CHECK_INTERVAL,
+  WORKER_URL
 } from "../../config";
 
 
 export const InjectionStatus: FC = () => {
-    const [loading, setLoading] = useState<boolean>(true)
-    const [status, setStatus] = useState<{ name: string, children: { name: string, status: boolean }[] }[] | null>(null)
-    const [APICollections, setAPICollections] = useState<APICollectionResource[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [status, setStatus] = useState<{ name: string, children: { name: string, status: boolean }[] }[] | null>(null)
+  const [APICollections, setAPICollections] = useState<APICollectionResource[]>([])
 
-    const isInjected = (res: string): boolean => res.search(INJECTED_CHECK_REGEXP) !== -1
+  const isInjected = (res: string): boolean => res.search(INJECTED_CHECK_REGEXP) !== -1
 
-    const updateStatus = (apis: APICollectionResource[]) => {
+  const updateStatus = (apis: APICollectionResource[]) => {
+    setLoading(true)
+    return Promise.all(apis.map(async api => ({
+      name: api.name,
+      children: await Promise.all(api.children.map(async child => ({
+        name: child.name,
+        status: await HttpClient.get({url: `${API_LAND_URL}/${api.path}/${child.path}`}).then((res) => isInjected(JSON.stringify(res)))
+      })))
+    })))
+    .then((value) => {
+      console.log(value)
+      setStatus(value)
+    })
+    .finally(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    HttpClient.get<APICollectionResource[]>({url: `${WORKER_URL}/api/apis`}).then((res) => {
+      setAPICollections(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    updateStatus(APICollections)
+
+    const updateInterval = setInterval(() => updateStatus(APICollections), INJECTION_CHECK_INTERVAL)
+    return () => clearInterval(updateInterval)
+
+  }, [APICollections, setAPICollections])
+
+  const injectAll = useCallback(() => {
         setLoading(true)
-        return Promise.all(apis.map(async api => ({
-            name: api.name,
-            children: await Promise.all(api.children.map(async child => ({
-                name: child.name,
-                status: await HttpClient.get({url: `${API_LAND_URL}/${api.path}/${child.path}`}).then((res) => isInjected(JSON.stringify(res)))
-            })))
-        })))
-        .then((value) => setStatus(value))
-        .finally(() => setLoading(false))
-    }
+        HttpClient.get({url: `${WORKER_URL}/api/inject/all`})
+        .then(() => updateStatus(APICollections))
+      },
+      [updateStatus]
+  )
 
-    useEffect(() => {
-        HttpClient.get<APICollectionResource[]>({url: `${WORKER_URL}/api/apis`}).then((res) => {
-            setAPICollections(res)
-            updateStatus(res)
-        })
-        const updateInterval = setInterval(() => updateStatus(APICollections), INJECTION_CHECK_INTERVAL)
-        return () => clearInterval(updateInterval)
-    }, [])
+  const resetAll = useCallback(() => {
+        setLoading(true)
 
-    const injectAll = useCallback(() => {
-            setLoading(true)
-            HttpClient.get({url: `${WORKER_URL}/api/inject/all`})
-            .then(() => updateStatus(APICollections))
-        },
-        [updateStatus]
-    )
+        HttpClient.get({url: `${WORKER_URL}/api/inject/resetAll`})
+        .then(() => updateStatus(APICollections))
+      },
+      [updateStatus]
+  )
 
-    const resetAll = useCallback(() => {
-            setLoading(true)
-
-            HttpClient.get({url: `${WORKER_URL}/api/inject/resetAll`})
-            .then(() => updateStatus(APICollections))
-        },
-        [updateStatus]
-    )
-
-    const headers = ['Land', 'Collections']
-    if (!status && loading) return <DataTableSkeleton headers={headers.map(header => ({header}))}/>
-    return (
-        <TableContainer
-            title="Injection status"
-            description="Monitor the injection status on the API Land">
-            <TableToolbar>
-                <TableToolbarContent>
-                    {loading && <div><InlineLoading style={{height: '100%'}}/></div>}
-                    <Button kind="secondary" onClick={resetAll}>
-                        Reset All APIs
-                    </Button>
-                    <Button kind="primary" onClick={injectAll}>
-                        Inject All APIs
-                    </Button>
-                </TableToolbarContent>
-            </TableToolbar>
-            <Table size="md" useZebraStyles={false}>
-                <TableHead>
-                    <TableRow>
-                        {headers.map((header) => (
-                            <TableHeader id={header} key={header}>
-                                {header}
-                            </TableHeader>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {status?.map((s) => (
-                        <TableRow key={s.name}>
-                            <TableCell>{s.name}</TableCell>
-                            <TableCell>
-                                {s.children.map((child) => (
-                                    <Tag key={child.name} type={child.status ? 'green' : 'red'}
-                                         size="md">
-                                        {child.name}
-                                    </Tag>
-                                ))}
-                            </TableCell>
-                        </TableRow>
+  const headers = ['Land', 'Collections']
+  if (!status && loading) return <DataTableSkeleton headers={headers.map(header => ({header}))}/>
+  return (
+      <TableContainer
+          title="Injection status"
+          description="Monitor the injection status on the API Land">
+        <TableToolbar>
+          <TableToolbarContent>
+            {loading && <div><InlineLoading style={{height: '100%'}}/></div>}
+            <Button kind="secondary" onClick={resetAll}>
+              Reset All APIs
+            </Button>
+            <Button kind="primary" onClick={injectAll}>
+              Inject All APIs
+            </Button>
+          </TableToolbarContent>
+        </TableToolbar>
+        <Table size="md" useZebraStyles={false}>
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                  <TableHeader id={header} key={header}>
+                    {header}
+                  </TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {status?.map((s) => (
+                <TableRow key={s.name}>
+                  <TableCell>{s.name}</TableCell>
+                  <TableCell>
+                    {s.children.map((child) => (
+                        <Tag key={child.name} type={child.status ? 'green' : 'red'}
+                             size="md">
+                          {child.name}
+                        </Tag>
                     ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+                  </TableCell>
+                </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+  )
 }
